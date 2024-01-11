20370ea3e1447523e1d4cb197387fce4cf49882c
xuannhat1203 <xuannhatvn211@gmail.com>
2024-01-11 16:41:18 +0700

    hello

--- B1.js
+++ B1.js
@@ -1,6 +1,12 @@
-let age = +prompt("NHap so tuoi cua ban");
-if(age > 0 && age <100){
-    console.log(age);
-}else{
+let year = +prompt("NHap nam sinh cua ban");
+let now = new Date();
+if(year < 0){
     alert("khong hop le");
+}else{
+    if( (now.getFullYear() - year < 0)){
+        console.log("khong hop le");
+    }else{
+        console.log("tuoi cua ban hien tai la:", now.getFullYear() - year );
+    }
+    
 }
